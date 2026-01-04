1cd24bee59f7b7daa29e2b1635399c5c4db5c717
Fede Secchi <federico.secchimarino@gmail.com>
2026-01-04 20:45:41 +0100

    chore: remove unused useEffect import from i18n context

--- src/i18n/context.tsx
+++ src/i18n/context.tsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState, ReactNode, useEffect } from 'react';
+import { createContext, useContext, useState, ReactNode } from 'react';
 import { en } from './locales/en';
 import { es } from './locales/es';
 
